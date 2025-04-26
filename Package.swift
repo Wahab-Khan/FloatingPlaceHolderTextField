8e3142a11e1a7b064626ba2c9e5a578c34f0bfed
Abdul Wahab Khan <awkhan313@gmail.com>
2025-04-26 11:04:48 +0500

    adding iOS platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "FloatingPlaceHolderTextField",
-    products: [
+    platforms: [
+        .iOS(.v16)
+    ], products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FloatingPlaceHolderTextField",
@@ -20,5 +22,6 @@ let package = Package(
             name: "FloatingPlaceHolderTextFieldTests",
             dependencies: ["FloatingPlaceHolderTextField"]
         ),
-    ]
+    ],
+
 )
---
+++ Sources/FloatingPlaceHolderTextField/SimpleTextField.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  FloatingPlaceHolderTextField
+//
+//  Created by Abdul Wahab Khan on 26/04/2025.
+//
+
